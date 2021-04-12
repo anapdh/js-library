c52d536964e8afd55ce09761912bacafe0ff43ac
Oever González <software@notengobattery.com>
2021-04-12 15:29:14 -0600

    Add the table header and a DOM function to show books

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ let myLibrary = [
   new Book("Goobye world", "The world", 2038)
 ];
 
-function Book(name, author, year) {
-  this.name = name;
+function Book(title, author, year) {
+  this.title = title;
   this.author = author;
   this.year = year;
   this.read = false;
@@ -12,4 +12,17 @@ function Book(name, author, year) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function showBooks(library) {
+  function createCell(parent, text) {
+    let cell = document.createElement("td");
+    cell.innerHTML = text;
+    parent.appendChild(cell);
+  }
+  let bookDOM = document.createElement("tr");
+  createCell(bookDOM, "My title");
+  createCell(bookDOM, "My author");
+  createCell(bookDOM, "1996");
+  bookShelf.appendChild(bookDOM);
 }
