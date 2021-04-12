de1d98fa16fc6e241332af85a2b18b9605470710
Oever González <software@notengobattery.com>
2021-04-12 15:01:40 -0600

    Add paramenters and attributes to the Book constructor

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(name, author, year) {
+  this.name = name;
+  this.author = author;
+  this.year = year;
+  this.read = false;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
