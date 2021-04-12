688b58214eeadf0d9e6361a0c04452d483265b83
Oever González <software@notengobattery.com>
2021-04-12 15:10:39 -0600

    Add sample books to the library array

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-let myLibrary = [];
+let myLibrary = [
+  new Book("Hello world", "The world", 1970),
+  new Book("Goobye world", "The world", 2038)
+];
 
 function Book(name, author, year) {
   this.name = name;
