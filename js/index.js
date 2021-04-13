8c41dcf3c964f9cdf657bf45f42362648479b5ab
anapdh <ana_d_hubner@hotmail.com>
2021-04-13 18:19:54 -0300

    Fix linters

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-let myLibrary = [
-  new Book("Hello world", "The world", 1970, 1000),
-  new Book("Goobye world", "The world", 2038, 2000)
-];
-
 function Book(title, author, year, pages) {
   this.title = title;
   this.author = author;
@@ -11,53 +6,58 @@ function Book(title, author, year, pages) {
   this.read = false;
 }
 
+const myLibrary = [
+  new Book('Hello world', 'The world', 1970, 1000),
+  new Book('Goobye world', 'The world', 2038, 2000),
+];
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
 function showBooks(library) {
   function createCell(parent, text) {
-    let cell = document.createElement("td");
+    const cell = document.createElement('td');
     cell.innerHTML = text;
     parent.appendChild(cell);
   }
-  bookShelfContent.innerHTML = '';
+  document.querySelector('#bookShelfContent').innerHTML = '';
   library.forEach((book, index) => {
-    let bookDOM = document.createElement("tr");
+    const bookDOM = document.createElement('tr');
     bookDOM.dataset.index = index;
     createCell(bookDOM, book.title);
     createCell(bookDOM, book.author);
     createCell(bookDOM, book.year);
     createCell(bookDOM, book.pages);
-    createCell(bookDOM, book.read ? "yes" : "no");
+    createCell(bookDOM, book.read ? 'yes' : 'no');
     createCell(bookDOM, '<a class="toggle-read" href="#">O</a>');
     createCell(bookDOM, '<a class="delete" href="#">X</a>');
-    bookShelfContent.appendChild(bookDOM);
+    document.querySelector('#bookShelfContent').appendChild(bookDOM);
   });
 }
 
 function deleteBooks(el) {
   if (el.classList.contains('delete')) {
-    let currentRow = el.parentElement.parentElement;
-    let currentIndex = Number(currentRow.dataset.index);
+    const currentRow = el.parentElement.parentElement;
+    const currentIndex = Number(currentRow.dataset.index);
     myLibrary.splice(currentIndex, 1);
   }
 }
 
 function toggleRead(el) {
   if (el.classList.contains('toggle-read')) {
-    let currentRow = el.parentElement.parentElement;
-    let currentIndex = Number(currentRow.dataset.index);
-    let book = myLibrary[currentIndex];
-    book.read ^= true; // XOR
+    const currentRow = el.parentElement.parentElement;
+    const currentIndex = Number(currentRow.dataset.index);
+    const book = myLibrary[currentIndex];
+    book.read = !book.read;
   }
 }
 
-function addBookHandler(event) {
-  const title = inputTitle.value;
-  const author = inputAuthor.value;
-  const year = inputYear.value;
-  const pages = inputPages.value;
+function addBookHandler() {
+  const title = document.querySelector('#inputTitle').value;
+  const author = document.querySelector('#inputAuthor').value;
+  const year = document.querySelector('#inputYear').value;
+  const pages = document.querySelector('#inputPages').value;
 
   const book = new Book(title, author, year, pages);
 
@@ -65,11 +65,11 @@ function addBookHandler(event) {
   showBooks(myLibrary);
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   showBooks(myLibrary);
-  inputButton.addEventListener("click", addBookHandler);
+  document.querySelector('#inputButton').addEventListener('click', addBookHandler);
 
-  bookShelfContent.addEventListener('click', (e) => {
+  document.querySelector('#bookShelfContent').addEventListener('click', (e) => {
     deleteBooks(e.target);
     toggleRead(e.target);
     showBooks(myLibrary);
